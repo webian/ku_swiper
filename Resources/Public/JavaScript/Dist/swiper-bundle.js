b1c6a6eeaa7030877d698bb580613ae00e557113
nanna-dk <nanna@e-nanna.dk>
2023-01-05 15:27:13 +0100

    Setup

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -2,18 +2,23 @@
  * Check for accessibility settings
  */
 const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+
 /**
  * Toggle SwiperJs play/pause button
  */
-const toggleBtnState = (swiperID) => {
-    if (reduceMotion.matches) {
+const toggleBtnState = (swiperID, el) => {
+    console.log(el);
+    if (isSliderActive || reduceMotion.matches) {
         swiperID.autoplay.stop();
-        isSliderActive = false;
-    } else if (isSliderActive) {
-        swiperID.autoplay.stop();
+        el.classList.add('paused');
+        el.setAttribute('aria-pressed', 'true');
+        el.setAttribute('aria-label', translations.play);
         isSliderActive = false;
     } else {
         swiperID.autoplay.start();
+        el.classList.remove('paused');
+        el.setAttribute('aria-pressed', 'false');
+        el.setAttribute('aria-label', translations.pause);
         isSliderActive = true;
     }
 }/**

c36e8281095af3b26578674fb3af441364baba82
nanna-dk <nanna@e-nanna.dk>
2023-01-10 13:58:20 +0100

    Setup

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -1,30 +1,26 @@
 /**
  * Check for accessibility settings
  */
-const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+//const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
 
 /**
  * Toggle Swiper play/pause button
- * @param swiper = Swiper object
+ * @param swiper = the Swiper object
  * @param el = the toggle button
  */
 const toggleBtnState = (swiper, el) => {
-    let isActive = el.classList.contains('paused');
+    let isRunning = swiper.autoplay.running;
     let icon = el.querySelector('.bi');
-    if (!isActive || reduceMotion.matches) {
+    if (isRunning) {
         swiper.autoplay.stop();
         el.setAttribute('aria-pressed', 'true');
         el.setAttribute('aria-label', translations.play);
-        el.classList.add('paused');
         icon.classList.replace('bi-pause-fill', 'bi-play-fill');
-        isActive = false;
     } else {
         swiper.autoplay.start();
         el.setAttribute('aria-pressed', 'false');
         el.setAttribute('aria-label', translations.pause);
-        el.classList.remove('paused');
         icon.classList.replace('bi-play-fill', 'bi-pause-fill');
-        isActive = true;
     }
 }
 

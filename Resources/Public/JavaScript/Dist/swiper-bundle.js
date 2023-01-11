f35ed6f11e960e3e2e2d70b0310f8e68f84a6a3c
nanna-dk <nanna@e-nanna.dk>
2023-01-11 15:48:05 +0100

    Setup

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -1,54 +1,52 @@
 /**
  * Swiper player button states
  */
-document.addEventListener('DOMContentLoaded', () => {
-    const play = 'bi-play-fill';
-    const pause = 'bi-pause-fill';
+const play = 'bi-play-fill';
+const pause = 'bi-pause-fill';
 
-    class SwiperState {
-        constructor(swiper, btn) {
-            this.swiper = swiper;
-            this.el = document.querySelector(btn);
-            this.icon = this.el.querySelector('.bi');
-            this.prefersReducedMotion();
-            this.addEventListeners();
-        }
+class SwiperState {
+    constructor(swiper, btn) {
+        this.swiper = swiper;
+        this.el = document.querySelector(btn);
+        this.icon = this.el.querySelector('.bi');
+        this.prefersReducedMotion();
+        this.addEventListeners();
+    }
 
-        addEventListeners() {
-            this.el.addEventListener('click', () => {
-                this.togglePlayPause();
-            });
-        }
+    addEventListeners() {
+        this.el.addEventListener('click', () => {
+            this.togglePlayPause();
+        });
+    }
 
-        togglePlayPause() {
-            if (this.swiper.autoplay.paused) {
-                this.playSwiper();
-            } else {
-                this.pauseSwiper();
-            }
-        }
-
-        playSwiper() {
-            this.swiper.autoplay.start();
-            this.icon.classList.replace(play, pause);
-            this.el.setAttribute('aria-pressed', 'false');
-            this.el.setAttribute('aria-label', translations.pause);
-        }
-
-        pauseSwiper() {
-            this.swiper.autoplay.stop();
-            this.icon.classList.replace(pause, play);
-            this.el.setAttribute('aria-pressed', 'true');
-            this.el.setAttribute('aria-label', translations.play);
-        }
-
-        prefersReducedMotion() {
-            if (this.swiper.autoplay.running === true && matchMedia('(prefers-reduced-motion)').matches) {
-                this.pauseSwiper();
-            }
+    togglePlayPause() {
+        if (this.swiper.autoplay.paused) {
+            this.playSwiper();
+        } else {
+            this.pauseSwiper();
         }
     }
-});/**
+
+    playSwiper() {
+        this.swiper.autoplay.start();
+        this.icon.classList.replace(play, pause);
+        this.el.setAttribute('aria-pressed', 'false');
+        this.el.setAttribute('aria-label', translations.pause);
+    }
+
+    pauseSwiper() {
+        this.swiper.autoplay.stop();
+        this.icon.classList.replace(pause, play);
+        this.el.setAttribute('aria-pressed', 'true');
+        this.el.setAttribute('aria-label', translations.play);
+    }
+
+    prefersReducedMotion() {
+        if (this.swiper.autoplay.running === true && matchMedia('(prefers-reduced-motion)').matches) {
+            this.pauseSwiper();
+        }
+    }
+}/**
  * Swiper 8.4.5
  * Most modern mobile touch slider and framework with hardware accelerated transitions
  * https://swiperjs.com

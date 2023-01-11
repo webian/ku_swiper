2274d0942a3c26e15782ff3baf6fbb5842123101
nanna-dk <nanna@e-nanna.dk>
2023-01-11 14:06:57 +0100

    Setup

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -1,40 +1,52 @@
-'use strict'
-
 /**
- * Check for accessibility settings
+ * Swiper player button states
  */
-//const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+const play = 'bi-play-fill';
+const pause = 'bi-pause-fill';
 
-/**
- * Toggle Swiper play/pause button
- * @param swiper = the Swiper object
- * @param el = the toggle button
- */
-const toggleBtnState = (swiper, el) => {
-    let isRunning = swiper.autoplay.running;
-    let icon = el.querySelector('.bi');
-    if (isRunning) {
-        swiper.autoplay.stop();
-        el.setAttribute('aria-pressed', 'true');
-        el.setAttribute('aria-label', translations.play);
-        icon.classList.replace('bi-pause-fill', 'bi-play-fill');
-    } else {
-        swiper.autoplay.start();
-        el.setAttribute('aria-pressed', 'false');
-        el.setAttribute('aria-label', translations.pause);
-        icon.classList.replace('bi-play-fill', 'bi-pause-fill');
+class SwiperState {
+    constructor(swiper, btn) {
+        this.swiper = swiper;
+        this.btn = btn;
+        this.el = document.querySelector(this.btn);
+        this.icon = this.el.querySelector('.bi');
+        this.prefersReducedMotion();
+        this.addEventListeners();
     }
-}
 
-/**
- * Get int for random initial slide
- * @param max: total number of slides
- */
-const getRandomInt = (max) => {
-    min = 0;
-    max = Math.floor(max);
-    // The maximum is inclusive and the minimum is inclusive
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    addEventListeners() {
+        this.el.addEventListener('click', () => {
+            this.togglePlayPause();
+        });
+    }
+
+    togglePlayPause() {
+        if (this.swiper.autoplay.paused) {
+            this.playSwiper();
+        } else {
+            this.pauseSwiper();
+        }
+    }
+
+    playSwiper() {
+        this.swiper.autoplay.start();
+        this.icon.classList.replace(play, pause);
+        this.el.setAttribute('aria-pressed', 'false');
+        this.el.setAttribute('aria-label', translations.pause);
+    }
+
+    pauseSwiper() {
+        this.swiper.autoplay.stop();
+        this.icon.classList.replace(pause, play);
+        this.el.setAttribute('aria-pressed', 'true');
+        this.el.setAttribute('aria-label', translations.play);
+    }
+
+    prefersReducedMotion() {
+        if (this.swiper.autoplay.running === true && matchMedia('(prefers-reduced-motion)').matches) {
+            this.pauseSwiper();
+        }
+    }
 }/**
  * Swiper 8.4.5
  * Most modern mobile touch slider and framework with hardware accelerated transitions
---
+++ Resources/Public/JavaScript/Src/ku-swiper-functions.js
@@ -0,0 +1,50 @@
+/**
+ * Swiper player button states
+ */
+const play = 'bi-play-fill';
+const pause = 'bi-pause-fill';
+
+class SwiperState {
+    constructor(swiper, btn) {
+        this.swiper = swiper;
+        this.btn = btn;
+        this.el = document.querySelector(this.btn);
+        this.icon = this.el.querySelector('.bi');
+        this.prefersReducedMotion();
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        this.el.addEventListener('click', () => {
+            this.togglePlayPause();
+        });
+    }
+
+    togglePlayPause() {
+        if (this.swiper.autoplay.paused) {
+            this.playSwiper();
+        } else {
+            this.pauseSwiper();
+        }
+    }
+
+    playSwiper() {
+        this.swiper.autoplay.start();
+        this.icon.classList.replace(play, pause);
+        this.el.setAttribute('aria-pressed', 'false');
+        this.el.setAttribute('aria-label', translations.pause);
+    }
+
+    pauseSwiper() {
+        this.swiper.autoplay.stop();
+        this.icon.classList.replace(pause, play);
+        this.el.setAttribute('aria-pressed', 'true');
+        this.el.setAttribute('aria-label', translations.play);
+    }
+
+    prefersReducedMotion() {
+        if (this.swiper.autoplay.running === true && matchMedia('(prefers-reduced-motion)').matches) {
+            this.pauseSwiper();
+        }
+    }
+}

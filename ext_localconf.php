3d7ac09f9c8866f88680f660bf5584aadac02ff6
nanna-dk <nanna@e-nanna.dk>
2023-01-19 15:45:33 +0100

    Setup

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,13 +9,15 @@
 
 defined('TYPO3') or die('Access denied.');
 
-use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+$typo3VersionNumber = VersionNumberUtility::convertVersionNumberToInteger(
+   VersionNumberUtility::getNumericTypo3Version()
+);
+
 // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
-if ($versionInformation->getMajorVersion() < 12) {
+if ($typo3VersionNumber < 12000000) {
     ExtensionManagementUtility::addPageTSConfig('
       @import "EXT:ku_swiper/Configuration/page.tsconfig"
    ');

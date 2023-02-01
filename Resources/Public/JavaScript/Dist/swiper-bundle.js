a1926e25735d2ba9ec1ec93a64196cb89e21f0d0
nanna-dk <nanna@e-nanna.dk>
2023-02-01 13:58:05 +0100

    Setup

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -5431,3 +5431,53 @@
 
 }));
 //# sourceMappingURL=swiper-bundle.js.map
+/**
+ * Swiper player button states
+ * @author Nanna Ellegaard
+ * @copyright 2023
+ */
+
+const play = 'bi-play-fill';
+const pause = 'bi-pause-fill';
+
+class SwiperState {
+    constructor(swiper, btn) {
+        this.swiper = swiper;
+        this.el = document.querySelector(btn);
+        this.icon = this.el.querySelector('.bi');
+        this.prefersReducedMotion();
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        this.el.addEventListener('click', () => {
+            this.togglePlayPause();
+        });
+    }
+
+    togglePlayPause() {
+        if (this.swiper.autoplay.running) {
+            this.pauseSwiper();
+        } else {
+            this.playSwiper();
+        }
+    }
+
+    playSwiper() {
+        this.swiper.autoplay.start();
+        this.icon.classList.replace(play, pause);
+        this.el.setAttribute('aria-label', translate.pause);
+    }
+
+    pauseSwiper() {
+        this.swiper.autoplay.stop();
+        this.icon.classList.replace(pause, play);
+        this.el.setAttribute('aria-label', translate.play);
+    }
+
+    prefersReducedMotion() {
+        if (this.swiper.autoplay.running && matchMedia('(prefers-reduced-motion)').matches) {
+            this.pauseSwiper();
+        }
+    }
+}

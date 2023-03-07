0ed409f0fec43c29f3ecb2cb9dea38491fb4666c
Nanna Ellegaard <nel@adm.ku.dk>
2023-03-07 13:45:16 +0100

    Setup

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -14,6 +14,6 @@ call_user_func(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
-        'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_title'
+        'KU Swiper'
     );
 });

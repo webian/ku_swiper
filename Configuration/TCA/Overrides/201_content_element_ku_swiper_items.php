7832a59fd347ac86fb49ca82549e34d2af2a2524
Nanna Ellegaard <nel@adm.ku.dk>
2023-03-09 11:35:30 +0100

    Setup

--- Configuration/TCA/Overrides/201_content_element_ku_swiper_items.php
+++ Configuration/TCA/Overrides/201_content_element_ku_swiper_items.php
@@ -9,94 +9,97 @@
 
 defined('TYPO3') or die('Access denied.');
 
-// Add Content Element
-if (!is_array($GLOBALS['TCA']['tt_content']['types']['ku_swiper'] ?? false)) {
-    $GLOBALS['TCA']['tt_content']['types']['ku_swiper'] = [];
-}
+call_user_func(function ($extKey ='ku_swiper', $contentType ='ku_swiper') {
+    // Add Content Element
+    if (!is_array($GLOBALS['TCA']['tt_content']['types'][$contentType] ?? false)) {
+        $GLOBALS['TCA']['tt_content']['types'][$contentType] = [];
+    }
 
-// Add content element PageTSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'ku_swiper',
-    'Configuration/TsConfig/Page/ku_swiper.tsconfig',
-    'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_title'
-);
-
-// Add content element to selector list
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
-    'tt_content',
-    'CType',
-    [
-        'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_title',
+    // Add content element PageTSConfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
         'ku_swiper',
-        'ku-swiper-icon',
-        'ku_swiper'
-    ]
-);
+        'Configuration/TsConfig/Page/ku_swiper.tsconfig',
+        'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_title'
+    );
 
-// Assign Icon
-$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['ku_swiper'] = 'ku-swiper-icon';
+    // Add content element to selector list
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        'tt_content',
+        'CType',
+        [
+            'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_title',
+            $contentType,
+            'ku-swiper-icon',
+            $extKey
+        ]
+    );
 
-// Configure element type
-$GLOBALS['TCA']['tt_content']['types']['ku_swiper'] = array_replace_recursive(
-    $GLOBALS['TCA']['tt_content']['types']['ku_swiper'],
-    [
-        'showitem' => '
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-                tx_ku_swiper_item,
-            --div--;LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:settings,
-                pi_flexform;LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:settings,
-            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-                --palette--;;language,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-                --palette--;;hidden,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-                categories,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-                rowDescription,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-        '
-    ]
-);
+    // Assign Icon
+    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes'][$contentType] = 'ku-swiper-icon';
 
-// Register fields
-$GLOBALS['TCA']['tt_content']['columns'] = array_replace_recursive(
-    $GLOBALS['TCA']['tt_content']['columns'],
-    [
-        'tx_ku_swiper_item' => [
-            'exclude' => 1,
-            'label' => 'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_item',
-            'config' => [
-                'type' => 'inline',
-                'minitems' => 1,
-                'foreign_table' => 'tx_ku_swiper_item',
-                'foreign_field' => 'tt_content',
-                'appearance' => [
-                    'useSortable' => true,
-                    'showSynchronizationLink' => true,
-                    'showAllLocalizationLink' => true,
-                    'showPossibleLocalizationRecords' => true,
-                    'expandSingle' => true,
-                    'enabledControls' => [
-                        'localize' => true,
+    // Configure element type
+    $GLOBALS['TCA']['tt_content']['types'][$contentType] = array_replace_recursive(
+        $GLOBALS['TCA']['tt_content']['types'][$contentType],
+        [
+            'showitem' => '
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+                    tx_ku_swiper_item,
+                --div--;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:settings,
+                    pi_flexform;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:settings,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    --palette--;;hidden,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                    rowDescription,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+            '
+        ]
+    );
+
+    // Register fields
+    $GLOBALS['TCA']['tt_content']['columns'] = array_replace_recursive(
+        $GLOBALS['TCA']['tt_content']['columns'],
+        [
+            'tx_ku_swiper_item' => [
+                'exclude' => 1,
+                'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_item_below',
+                'config' => [
+                    'type' => 'inline',
+                    'minitems' => 1,
+                    'foreign_table' => 'tx_ku_swiper_item',
+                    'foreign_field' => 'tt_content',
+                    'appearance' => [
+                        'newRecordLinkTitle' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_item',
+                        'useSortable' => true,
+                        'showSynchronizationLink' => true,
+                        'showAllLocalizationLink' => true,
+                        'showPossibleLocalizationRecords' => true,
+                        'expandSingle' => true,
+                        'enabledControls' => [
+                            'localize' => true,
+                        ]
+                    ],
+                    'behaviour' => [
+                        'mode' => 'select',
                     ]
-                ],
-                'behaviour' => [
-                    'mode' => 'select',
                 ]
             ]
         ]
-    ]
-);
+    );
 
-// Add flexForms for content element configuration
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:ku_swiper/Configuration/Flexforms/default.xml',
-    'ku_swiper'
-);
+    // Add flexForms for content element configuration
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:' . $extKey . '/Configuration/Flexforms/default.xml',
+        $contentType
+    );
+});
--- Configuration/TCA/Overrides/202_content_element_ku_swiper_content.php
+++ Configuration/TCA/Overrides/202_content_element_ku_swiper_content.php
@@ -9,64 +9,66 @@
 
 defined('TYPO3') or die('Access denied.');
 
-// Add Content Element
-if (!is_array($GLOBALS['TCA']['tt_content']['types']['ku_swiper_content'] ?? false)) {
-    $GLOBALS['TCA']['tt_content']['types']['ku_swiper_content'] = [];
-}
+call_user_func(function ($extKey ='ku_swiper', $contentType ='ku_swiper_content') {
+    // Add Content Element
+    if (!is_array($GLOBALS['TCA']['tt_content']['types'][$contentType] ?? false)) {
+        $GLOBALS['TCA']['tt_content']['types'][$contentType] = [];
+    }
 
-// Add content element PageTSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'ku_swiper',
-    'Configuration/TsConfig/Page/ku_swiper.tsconfig',
-    'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_content_title'
-);
+    // Add content element PageTSConfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'ku_swiper',
+        'Configuration/TsConfig/Page/ku_swiper.tsconfig',
+        'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_content_title'
+    );
 
-// Add content element to selector list
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
-    'tt_content',
-    'CType',
-    [
-        'LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_content_title',
-        'ku_swiper_content',
-        'ku-swiper-icon',
-        'ku_swiper'
-    ]
-);
+    // Add content element to selector list
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        'tt_content',
+        'CType',
+        [
+            'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_content_title',
+            $contentType,
+            'ku-swiper-icon',
+            'ku_swiper'
+        ]
+    );
 
-// Assign Icon
-$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['ku_swiper_content'] = 'ku-swiper-icon';
+    // Assign Icon
+    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes'][$contentType] = 'ku-swiper-icon';
 
-// Configure element type
-$GLOBALS['TCA']['tt_content']['types']['ku_swiper_content'] = array_replace_recursive(
-    $GLOBALS['TCA']['tt_content']['types']['ku_swiper_content'],
-    [
-        'showitem' => '
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-                records;LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:swiper_records,
-            --div--;LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:settings,
-                pi_flexform;LLL:EXT:ku_swiper/Resources/Private/Language/locallang_be.xlf:settings,
-            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-                --palette--;;language,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-                --palette--;;hidden,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-                categories,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-                rowDescription,
-            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-        '
-    ]
-);
+    // Configure element type
+    $GLOBALS['TCA']['tt_content']['types'][$contentType] = array_replace_recursive(
+        $GLOBALS['TCA']['tt_content']['types'][$contentType],
+        [
+            'showitem' => '
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+                    records;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:swiper_records,
+                --div--;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:settings,
+                    pi_flexform;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:settings,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    --palette--;;hidden,
+                    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                    rowDescription,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+            '
+        ]
+    );
 
-// Add flexForms for content element configuration
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:ku_swiper/Configuration/Flexforms/default.xml',
-    'ku_swiper_content'
-);
+    // Add flexForms for content element configuration
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:' . $extKey . '/Configuration/Flexforms/default.xml',
+        $contentType
+    );
+});

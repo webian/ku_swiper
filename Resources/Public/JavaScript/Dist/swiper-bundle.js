56c48d84abd1403d06ce661d5434e99ca603f7a8
Nanna Ellegaard <nel@adm.ku.dk>
2023-04-11 10:37:23 +0200

    Bump version

--- Resources/Public/JavaScript/Dist/swiper-bundle.js
+++ Resources/Public/JavaScript/Dist/swiper-bundle.js
@@ -46,7 +46,7 @@ class SwiperState {
         }
     }
 }/**
- * Swiper 9.1.1
+ * Swiper 9.2.0
  * Most modern mobile touch slider and framework with hardware accelerated transitions
  * https://swiperjs.com
  *
@@ -54,7 +54,7 @@ class SwiperState {
  *
  * Released under the MIT License
  *
- * Released on: March 16, 2023
+ * Released on: April 11, 2023
  */
 
 (function (global, factory) {
@@ -1337,6 +1337,41 @@ class SwiperState {
       swiper.emitSlidesClasses();
     }
 
+    const processLazyPreloader = (swiper, imageEl) => {
+      if (!swiper || swiper.destroyed || !swiper.params) return;
+      const slideSelector = () => swiper.isElement ? `swiper-slide` : `.${swiper.params.slideClass}`;
+      const slideEl = imageEl.closest(slideSelector());
+      if (slideEl) {
+        const lazyEl = slideEl.querySelector(`.${swiper.params.lazyPreloaderClass}`);
+        if (lazyEl) lazyEl.remove();
+      }
+    };
+    const unlazy = (swiper, index) => {
+      if (!swiper.slides[index]) return;
+      const imageEl = swiper.slides[index].querySelector('[loading="lazy"]');
+      if (imageEl) imageEl.removeAttribute('loading');
+    };
+    const preload = swiper => {
+      if (!swiper || swiper.destroyed || !swiper.params) return;
+      let amount = swiper.params.lazyPreloadPrevNext;
+      const len = swiper.slides.length;
+      if (!len || !amount || amount < 0) return;
+      amount = Math.min(amount, len);
+      const slidesPerView = swiper.params.slidesPerView === 'auto' ? swiper.slidesPerViewDynamic() : Math.ceil(swiper.params.slidesPerView);
+      const activeIndex = swiper.activeIndex;
+      const slideIndexLastInView = activeIndex + slidesPerView - 1;
+      if (swiper.params.rewind) {
+        for (let i = activeIndex - amount; i <= slideIndexLastInView + amount; i += 1) {
+          const realIndex = (i % len + len) % len;
+          if (realIndex !== activeIndex && realIndex > slideIndexLastInView) unlazy(swiper, realIndex);
+        }
+      } else {
+        for (let i = Math.max(slideIndexLastInView - amount, 0); i <= Math.min(slideIndexLastInView + amount, len - 1); i += 1) {
+          if (i !== activeIndex && i > slideIndexLastInView) unlazy(swiper, i);
+        }
+      }
+    };
+
     function getActiveIndexByTranslate(swiper) {
       const {
         slidesGrid,
@@ -1418,6 +1453,9 @@ class SwiperState {
         previousIndex,
         activeIndex
       });
+      if (swiper.initialized) {
+        preload(swiper);
+      }
       swiper.emit('activeIndexChange');
       swiper.emit('snapIndexChange');
       if (previousRealIndex !== realIndex) {
@@ -2915,16 +2953,6 @@ class SwiperState {
       swiper.emit('setTranslate', swiper.translate, false);
     }
 
-    const processLazyPreloader = (swiper, imageEl) => {
-      if (!swiper || swiper.destroyed || !swiper.params) return;
-      const slideSelector = () => swiper.isElement ? `swiper-slide` : `.${swiper.params.slideClass}`;
-      const slideEl = imageEl.closest(slideSelector());
-      if (slideEl) {
-        const lazyEl = slideEl.querySelector(`.${swiper.params.lazyPreloaderClass}`);
-        if (lazyEl) lazyEl.remove();
-      }
-    };
-
     function onLoad(e) {
       const swiper = this;
       processLazyPreloader(swiper, e.target);
@@ -3340,6 +3368,7 @@ class SwiperState {
       slidePrevClass: 'swiper-slide-prev',
       wrapperClass: 'swiper-wrapper',
       lazyPreloaderClass: 'swiper-lazy-preloader',
+      lazyPreloadPrevNext: 0,
       // Callbacks
       runCallbacksOnInit: true,
       // Internals
@@ -3515,7 +3544,7 @@ class SwiperState {
             // Form elements to match
             focusableElements: swiper.params.focusableElements,
             // Last click time
-            lastClickTime: now(),
+            lastClickTime: 0,
             clickTimeout: undefined,
             // Velocities
             velocities: [],
@@ -3871,9 +3900,11 @@ class SwiperState {
             });
           }
         });
+        preload(swiper);
 
         // Init Flag
         swiper.initialized = true;
+        preload(swiper);
 
         // Emit
         swiper.emit('init');
